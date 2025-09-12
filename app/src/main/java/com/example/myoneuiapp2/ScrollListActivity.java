ff3f89cdaad9c706f73cd897f4f4b0978d5ffe4f
tamimi19 <mohamad.tamimii.82@gmail.com>
2025-09-12 22:01:07 +0300

    Create ScrollListActivity.java

---
+++ app/src/main/java/com/example/myoneuiapp2/ScrollListActivity.java
@@ -0,0 +1,41 @@
+package com.example.myoneuiapp2;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.view.MenuItem;
+
+public class ScrollListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scroll_list);
+
+        Toolbar toolbar = findViewById(R.id.toolbar_scroll);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // ملء القائمة بـ70 عنصر
+        String[] items = new String[70];
+        for (int i = 0; i < 70; i++) {
+            items[i] = "Item " + (i + 1);
+        }
+        ListView listView = findViewById(R.id.list_view);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+            android.R.layout.simple_list_item_1, items);
+        listView.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // معالجة زر العودة في شريط الأدوات
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
