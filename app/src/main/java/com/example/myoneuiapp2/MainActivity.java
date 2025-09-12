8deb7eb6c9b4c5f0e4d31d35f1eaf67810d91f64
tamimi19 <mohamad.tamimii.82@gmail.com>
2025-09-12 22:00:21 +0300

    Create MainActivity.java

---
+++ app/src/main/java/com/example/myoneuiapp2/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.myoneuiapp2;
+
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import android.view.Gravity;
+import android.view.MenuItem;
+import com.google.android.material.navigation.NavigationView;
+import androidx.appcompat.widget.Toolbar;
+import android.content.Intent;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+
+        // إعداد أيقونة القائمة لفتح درج التنقل على اليمين
+        toolbar.setNavigationIcon(android.R.drawable.ic_menu_sort_by_size);
+        toolbar.setNavigationOnClickListener(v -> drawerLayout.openDrawer(Gravity.END));
+
+        // التعامل مع اختيار عناصر القائمة الجانبية
+        navigationView.setNavigationItemSelectedListener(menuItem -> {
+            drawerLayout.closeDrawer(Gravity.END);
+            int id = menuItem.getItemId();
+            if (id == R.id.nav_settings) {
+                startActivity(new Intent(MainActivity.this, SettingsActivity.class));
+            } else if (id == R.id.nav_scroll_list) {
+                startActivity(new Intent(MainActivity.this, ScrollListActivity.class));
+            }
+            return true;
+        });
+    }
+}
