e5f42ecfe897295e9ee9fd1365889aafc58fefc2
tamimi19 <mohamad.tamimii.82@gmail.com>
2025-09-12 22:02:37 +0300

    Create SettingsActivity.java

---
+++ app/src/main/java/com/example/myoneuiapp2/SettingsActivity.java
@@ -0,0 +1,34 @@
+package com.example.myoneuiapp2;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import android.view.MenuItem;
+
+public class SettingsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        Toolbar toolbar = findViewById(R.id.toolbar_settings);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(R.string.title_settings);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // عرض واجهة التفضيلات
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.settings_container, new SettingsFragment())
+                .commit();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // زر العودة
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
