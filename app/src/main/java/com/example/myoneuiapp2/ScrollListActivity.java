328b7c7c0e5357aaf93ed9bb9243954cb3e97b22
tamimi19 <mohamad.tamimii.82@gmail.com>
2025-09-14 19:15:39 +0300

    Update ScrollListActivity.java

--- app/src/main/java/com/example/myoneuiapp2/ScrollListActivity.java
+++ app/src/main/java/com/example/myoneuiapp2/ScrollListActivity.java
@@ -1,8 +1,8 @@
 package com.example.myoneuiapp2;
 
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+import io.github.oneuiproject.sesl.support.v7.app.AppCompatActivity;
+import io.github.oneuiproject.sesl.support.v7.widget.Toolbar;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.view.MenuItem;
